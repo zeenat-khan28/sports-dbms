ad03754454d2a58c18b12d8075b324e9d3dbb284
zeenat-khan28 <khan4zeenat@gmail.com>
2026-01-20 12:00:21 +0530

    yayyy done with docker and ngnix

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -3,7 +3,9 @@
  */
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:8000/api';
+const API_BASE_URL = import.meta.env.VITE_API_URL
+    ? `${import.meta.env.VITE_API_URL}/api`
+    : 'http://localhost:8000/api';
 
 // Create axios instance
 const api = axios.create({
